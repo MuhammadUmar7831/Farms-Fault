c4ab4fd6676a2ed2592aff76da0b14443a372d7a
MuhammadKhizar89 <muhammadkhizar8919@gmail.com>
2024-10-12 15:26:52 +0500

    Points in Select Option Added

--- client/src/components/reportAnError/SelectError.jsx
+++ client/src/components/reportAnError/SelectError.jsx
@@ -33,11 +33,11 @@ function SelectError({type,errors,setType, setPoints}) {
                 </option>
                 {errorTypes.map((errorType, index) => (
                     <option key={index} value={JSON.stringify({type: errorType.name, points: errorType.points})}>
-                        {errorType.name}
+                        {errorType.name} - {errorType.points} Points
                     </option>
                 ))}
             </select>
-            <div className="absolute inset-y-7 right-0 flex items-center pointer-events-none">
+            <div className="absolute inset-y-7 right-0 flex items-center pointer-events-none ">
                 <svg
                     className="w-5 h-5 text-gray-400"
                     xmlns="http://www.w3.org/2000/svg"
