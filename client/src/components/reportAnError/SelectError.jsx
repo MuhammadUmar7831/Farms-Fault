b6750d98c0218d85345d3a27942bedb2a4f84204
MuhammadKhizar89 <muhammadkhizar8919@gmail.com>
2024-10-12 16:01:36 +0500

    Select svg Fixed

--- client/src/components/reportAnError/SelectError.jsx
+++ client/src/components/reportAnError/SelectError.jsx
@@ -39,7 +39,7 @@ function SelectError({type,errors,setType, setPoints}) {
             </select>
             <div className="absolute inset-y-7 right-0 flex items-center pointer-events-none ">
                 <svg
-                    className="w-5 h-5 text-gray-400"
+                    className="w-5 h-5 text-gray-400 bg-primary"
                     xmlns="http://www.w3.org/2000/svg"
                     fill="none"
                     viewBox="0 0 24 24"
